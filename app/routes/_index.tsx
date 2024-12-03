b51f54c590b924b74f258c4c75356667b2d505c8
Ardeman <are.theman@gmail.com>
2024-12-04 03:43:04 +0800

    Enhance meta and links for the index route with updated title, description, and favicon

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,13 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+export const meta: MetaFunction = () => [
+    { title: "Circle Sync" },
+    { name: "description", content: "A comprehensive personal productivity app featuring task lists, timers, and money logging. Organize your tasks, stay on top of your schedule, and track your finances efficiently." },
   ];
-};
+
+export const links: LinksFunction = () => [
+  { rel: "icon", href: "./icon/favicon.ico" },
+]
 
 export default function Index() {
   return (
