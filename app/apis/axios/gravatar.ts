7917aa9fe0ddce2dafe2e4bce6ac5eebb48581d4
Ardeman <are.theman@gmail.com>
2024-12-14 14:11:39 +0800

    Remove Gravatar API integration and related environment variable

--- app/apis/axios/gravatar.ts
+++
@@ -1,8 +0,0 @@
-export const test = async () => {
-  const data = await fetch('https://api.gravatar.com/v3/profiles', {
-    headers: {
-      authorization: `Bearer ${import.meta.env.VITE_GRAVATAR_API_KEY as string}`,
-    },
-  })
-  return { data }
-}
