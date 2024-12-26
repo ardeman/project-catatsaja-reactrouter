96534a555d9f541d0a4eecd8a8b84bc5c644af5b
Ardeman <are.theman@gmail.com>
2024-12-26 19:00:46 +0800

    Add money log API and types for fetching money logs

---
+++ app/apis/firestore/money-log.ts
@@ -0,0 +1,38 @@
+import {
+  collection,
+  FieldPath,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore'
+
+import { auth, firestore } from '~/lib/configs'
+import { TMoneyLogResponse } from '~/lib/types'
+
+export const fetchMoneyLogs = async () => {
+  if (!firestore) {
+    throw new Error('Firebase DB is not initialized')
+  }
+  if (!auth?.currentUser) {
+    throw new Error('No user is currently signed in.')
+  }
+
+  const queryRef = query(
+    collection(firestore, 'money-logs'),
+    where(
+      new FieldPath('permissions', 'read'),
+      'array-contains',
+      auth.currentUser.uid,
+    ),
+  )
+  const snap = await getDocs(queryRef)
+
+  return snap.docs.map((doc) => {
+    const data = doc.data()
+    return {
+      ...data,
+      id: doc.id,
+      isPinned: data.pinnedBy?.includes(auth?.currentUser?.uid),
+    } as TMoneyLogResponse
+  })
+}
---
+++ app/lib/types/money-log.ts
@@ -0,0 +1,21 @@
+import { TPermissions, TTime } from './common'
+
+export type TMoneyLogResponse = {
+  id: string
+  title: string
+  isPinned?: boolean
+  pinnedBy?: string[]
+  createdAt: TTime
+  updatedAt: TTime
+  owner: string
+  permissions?: TPermissions
+}
+
+export type TLogResponse = {
+  id: string
+  description: string
+  amount: number
+  currency: string
+  rate: number
+  timestamp: TTime
+}
