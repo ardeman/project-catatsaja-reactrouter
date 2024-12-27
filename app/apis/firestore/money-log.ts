cb71c84b9667c2b8349a6072a1e2fdcb976933e4
Ardeman <are.theman@gmail.com>
2024-12-27 13:12:05 +0800

    Add fetchLogs function to retrieve logs for a specific money log

--- app/apis/firestore/money-log.ts
+++ app/apis/firestore/money-log.ts
@@ -7,7 +7,7 @@ import {
 } from 'firebase/firestore'
 
 import { auth, firestore } from '~/lib/configs'
-import { TMoneyLogResponse } from '~/lib/types'
+import { TLogResponse, TMoneyLogResponse } from '~/lib/types'
 
 export const fetchMoneyLogs = async () => {
   if (!firestore) {
@@ -34,5 +34,25 @@ export const fetchMoneyLogs = async () => {
       id: doc.id,
       isPinned: data.pinnedBy?.includes(auth?.currentUser?.uid),
     } as TMoneyLogResponse
+  })
+}
+
+export const fetchLogs = async (id: string) => {
+  if (!firestore) {
+    throw new Error('Firebase DB is not initialized')
+  }
+  if (!auth?.currentUser) {
+    throw new Error('No user is currently signed in.')
+  }
+
+  const queryRef = query(collection(firestore, 'money-logs', id, 'logs'))
+  const snap = await getDocs(queryRef)
+
+  return snap.docs.map((doc) => {
+    const data = doc.data()
+    return {
+      ...data,
+      id: doc.id,
+    } as TLogResponse
   })
 }
