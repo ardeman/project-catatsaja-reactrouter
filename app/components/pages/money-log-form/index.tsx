cc71040560dba4773c0c5feb26867d9ac1fd076a
Ardeman <are.theman@gmail.com>
2024-12-29 21:22:25 +0800

    Refactor money log components to remove wrapper and integrate ItemForm and TitleForm directly

--- app/components/pages/money-log-form/index.tsx
+++ app/components/pages/money-log-form/index.tsx
@@ -1,10 +1,14 @@
 import { MoneyLogProvider } from './context'
-import { Wrapper } from './wrapper'
+import { ItemForm } from './item-form'
+import { TitleForm } from './title-form'
 
 export const MoneyLogFormPage = () => {
   return (
     <MoneyLogProvider>
-      <Wrapper />
+      <main className="flex flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-8">
+        <TitleForm />
+        <ItemForm />
+      </main>
     </MoneyLogProvider>
   )
 }
