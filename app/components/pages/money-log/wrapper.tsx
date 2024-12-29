92d20c1674d243f309f9e8db83f7a4866e15fb36
Ardeman <are.theman@gmail.com>
2024-12-29 19:12:24 +0800

    Refactor money log wrapper to use Link for navigation and simplify button structure

--- app/components/pages/money-log/wrapper.tsx
+++ app/components/pages/money-log/wrapper.tsx
@@ -1,23 +1,21 @@
+import { Link } from '@remix-run/react'
 import { useTranslation } from 'react-i18next'
 
-import { Button } from '~/components/base'
-import { Card, CardContent, CardTitle } from '~/components/ui'
-
-import { useMoneyLog } from './context'
+import { Button, Card, CardContent, CardTitle } from '~/components/ui'
 
 export const Wrapper = () => {
   const { t } = useTranslation()
-  const { handleCreateMoneyLog } = useMoneyLog()
 
   return (
     <main className="flex flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-8">
-      <Button
-        containerClassName="flex justify-center md:static md:transform-none fixed bottom-4 left-1/2 -translate-x-1/2 z-50"
-        className="w-full max-w-md"
-        onClick={handleCreateMoneyLog}
-      >
-        {t('moneyLog.add')}
-      </Button>
+      <div className="fixed bottom-4 left-1/2 z-50 flex -translate-x-1/2 justify-center md:static md:transform-none">
+        <Button
+          className="w-full max-w-md"
+          asChild
+        >
+          <Link to="/dashboard/money-log/add">{t('moneyLog.add')}</Link>
+        </Button>
+      </div>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
         <Card>
           <CardTitle>Title</CardTitle>
