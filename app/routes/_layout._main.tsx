54bee9523e385426098da0ce6017aa8313c8c860
Ardeman <are.theman@gmail.com>
2025-07-14 18:46:42 +0800

    feat(dashboard): integrate user data handling for theme and language settings

--- app/routes/_layout._main.tsx
+++ app/routes/_layout._main.tsx
@@ -1,7 +1,11 @@
+import { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
 import { Outlet } from 'react-router'
 
 import { fetchUserData } from '~/apis/firestore/user'
 import { Navbar } from '~/components/layouts/navbar'
+import { useTheme } from '~/lib/contexts/theme'
+import { useUserData } from '~/lib/hooks/use-get-user'
 
 export const clientLoader = async () => {
   try {
@@ -12,6 +16,16 @@ export const clientLoader = async () => {
 }
 
 const Dashboard = () => {
+  const { data: userData } = useUserData()
+  const { setTheme, theme } = useTheme()
+  const { i18n } = useTranslation()
+
+  useEffect(() => {
+    if (userData?.theme && theme !== userData.theme) setTheme(userData.theme)
+    if (userData?.language && i18n.language !== userData.language)
+      i18n.changeLanguage(userData.language)
+  }, [userData, setTheme, i18n, theme])
+
   return (
     <main className="flex min-h-dvh w-full flex-col">
       <Navbar />
