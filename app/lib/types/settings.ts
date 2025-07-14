8c6bbf55a7e756b036fbab948a0e8e7e95bfad19
Ardeman <are.theman@gmail.com>
2025-07-14 16:00:50 +0800

    feat(settings): integrate react-hook-form for appearance

--- app/lib/types/settings.ts
+++ app/lib/types/settings.ts
@@ -1,5 +1,11 @@
 import { z } from 'zod'
 
+import { Theme } from '~/lib/contexts/theme'
 import { generalSettingSchema } from '~/lib/validations/settings'
 
 export type TUpdateProfileRequest = z.infer<typeof generalSettingSchema>
+
+export type TUpdateAppearanceRequest = {
+  theme: Theme
+  language: string
+}
