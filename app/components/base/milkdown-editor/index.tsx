eb8d1ddec1423471a33cb1f2ddc92298d1e65b9c
Ardeman <are.theman@gmail.com>
2025-07-20 00:48:09 +0800

    feat(note): show placeholder and selected content

--- app/components/base/milkdown-editor/index.tsx
+++ app/components/base/milkdown-editor/index.tsx
@@ -3,6 +3,7 @@ import { commonmark } from '@milkdown/kit/preset/commonmark'
 import { listener, listenerCtx } from '@milkdown/plugin-listener'
 import { Milkdown, useEditor } from '@milkdown/react'
 import { nord } from '@milkdown/theme-nord'
+import { placeholder, placeholderCtx } from 'milkdown-plugin-placeholder'
 import { useEffect } from 'react'
 import { PathValue, useFormContext } from 'react-hook-form'
 
@@ -11,7 +12,7 @@ import { TMilkdownEditorProperties } from './type'
 export const MilkdownEditor = <TFormValues extends Record<string, unknown>>(
   properties: TMilkdownEditorProperties<TFormValues>,
 ) => {
-  const { name } = properties
+  const { name, placeholder: placeholderText } = properties
   const { register, setValue, getValues } = useFormContext<TFormValues>()
 
   useEffect(() => {
@@ -24,6 +25,9 @@ export const MilkdownEditor = <TFormValues extends Record<string, unknown>>(
         .config((context) => {
           context.set(rootCtx, root)
           context.set(defaultValueCtx, (getValues(name) as string) || '')
+          if (placeholderText) {
+            context.set(placeholderCtx, placeholderText)
+          }
           context.get(listenerCtx).markdownUpdated((_, markdown) => {
             setValue(name, markdown as PathValue<TFormValues, typeof name>, {
               shouldDirty: true,
@@ -32,8 +36,9 @@ export const MilkdownEditor = <TFormValues extends Record<string, unknown>>(
         })
         .config(nord)
         .use(commonmark)
-        .use(listener),
-    [name, getValues, setValue],
+        .use(listener)
+        .use(placeholder),
+    [name, getValues, setValue, placeholderText],
   )
 
   return <Milkdown />
--- app/components/base/milkdown-editor/type.ts
+++ app/components/base/milkdown-editor/type.ts
@@ -2,4 +2,5 @@ import { FieldValues, Path } from 'react-hook-form'
 
 export type TMilkdownEditorProperties<TFormValues extends FieldValues> = {
   name: Path<TFormValues>
+  placeholder?: string
 }
