7d2005c3d2aa1ce48714e5a776128a7344a4c02e
Ardeman <are.theman@gmail.com>
2025-07-20 02:34:56 +0800

    feat: enhance milkdown

--- app/components/base/milkdown-editor/index.tsx
+++ app/components/base/milkdown-editor/index.tsx
@@ -1,13 +1,12 @@
-import { Editor, defaultValueCtx, rootCtx } from '@milkdown/kit/core'
-import { commonmark } from '@milkdown/kit/preset/commonmark'
-import { listener, listenerCtx } from '@milkdown/plugin-listener'
+import { Crepe } from '@milkdown/crepe'
 import { Milkdown, useEditor } from '@milkdown/react'
-import { nord } from '@milkdown/theme-nord'
-import { placeholder, placeholderCtx } from 'milkdown-plugin-placeholder'
 import { useEffect } from 'react'
 import { PathValue, useFormContext } from 'react-hook-form'
 
 import { TMilkdownEditorProperties } from './type'
+
+import '@milkdown/crepe/theme/common/style.css'
+import '~/styles/crepe.css'
 
 export const MilkdownEditor = <TFormValues extends Record<string, unknown>>(
   properties: TMilkdownEditorProperties<TFormValues>,
@@ -19,26 +18,34 @@ export const MilkdownEditor = <TFormValues extends Record<string, unknown>>(
     register(name)
   }, [register, name])
 
-  useEditor(
-    (root) =>
-      Editor.make()
-        .config((context) => {
-          context.set(rootCtx, root)
-          context.set(defaultValueCtx, (getValues(name) as string) || '')
-          if (placeholderText) {
-            context.set(placeholderCtx, placeholderText)
-          }
-          context.get(listenerCtx).markdownUpdated((_, markdown) => {
-            setValue(name, markdown as PathValue<TFormValues, typeof name>, {
-              shouldDirty: true,
-            })
-          })
+  useEditor((root) =>
+    new Crepe({
+      root,
+      defaultValue: (getValues(name) as string) || '',
+      features: {
+        // [Crepe.Feature.Cursor]: true,
+        // [Crepe.Feature.ListItem]: true,
+        // [Crepe.Feature.LinkTooltip]: true,
+        // [Crepe.Feature.ImageBlock]: true,
+        [Crepe.Feature.BlockEdit]: false,
+        // [Crepe.Feature.Toolbar]: true,
+        // [Crepe.Feature.CodeMirror]: true,
+        // [Crepe.Feature.Table]: true,
+        // [Crepe.Feature.Latex]: true,
+      },
+      featureConfigs: {
+        [Crepe.Feature.Placeholder]: {
+          text: placeholderText,
+          mode: 'doc',
+        },
+      },
+    }).on((listener) => {
+      listener.markdownUpdated((_, markdown) => {
+        setValue(name, markdown as PathValue<TFormValues, typeof name>, {
+          shouldDirty: true,
         })
-        .config(nord)
-        .use(commonmark)
-        .use(listener)
-        .use(placeholder),
-    [name, getValues, setValue, placeholderText],
+      })
+    }),
   )
 
   return <Milkdown />
