519560b9c974453c7e07078c2f4ced9993f7ff52
Ardeman <are.theman@gmail.com>
2025-07-20 23:53:36 +0800

    chore: add color to brand name

--- app/components/pages/about/index.tsx
+++ app/components/pages/about/index.tsx
@@ -1,4 +1,4 @@
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 
 import { appName } from '~/lib/constants/metadata'
 
@@ -7,7 +7,13 @@ export const AboutPage = () => {
   return (
     <div className="mx-auto grid min-h-fit max-w-md gap-4 p-4 md:gap-8 md:p-8 md:pt-0">
       <h1 className="text-3xl font-semibold">{t('navigation.about')}</h1>
-      <p>{t('about.description', { appName })}</p>
+      <p>
+        <Trans
+          i18nKey="about.description"
+          values={{ appName }}
+          components={{ span: <span className="text-primary" /> }}
+        />
+      </p>
     </div>
   )
 }
