82dcc042fa70cde4b74607b765d302af9bc742b2
Ardeman <are.theman@gmail.com>
2025-07-26 00:24:43 +0800

    refactor: enhance sidebar styling and link behavior

--- app/components/layouts/sidebar/index.tsx
+++ app/components/layouts/sidebar/index.tsx
@@ -1,20 +1,25 @@
 import { useTranslation } from 'react-i18next'
 import { Link, useLocation } from 'react-router'
 
+import { cn } from '~/lib/utils/shadcn'
+
 import { settings } from './constant'
 
 export const Sidebar = () => {
   const { pathname } = useLocation()
   const { t } = useTranslation()
   return (
-    <nav className="flex gap-4 text-sm text-muted-foreground md:grid">
+    <nav className="inline-flex w-fit items-center justify-center gap-2 rounded-lg bg-muted p-1 text-muted-foreground md:w-full md:flex-col">
       {settings(t).map((setting) => (
         <Link
           key={setting.name}
           to={setting.href}
-          className={
-            pathname === setting.href ? 'font-semibold text-primary' : ''
-          }
+          className={cn(
+            'inline-flex flex-1 items-center justify-center whitespace-nowrap rounded-md border border-transparent px-2 py-1 font-medium text-foreground transition-[color,box-shadow] focus-visible:border-ring focus-visible:outline-1 focus-visible:outline-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 dark:text-muted-foreground md:w-full md:justify-start',
+            pathname === setting.href
+              ? 'bg-background shadow-sm dark:border-input dark:bg-card/30 dark:text-foreground'
+              : '',
+          )}
         >
           {setting.name}
         </Link>
